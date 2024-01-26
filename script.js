b98d70457eb938de64523309f2f657573d0de18e
Dibya <dibyaranjansahu.mail@gmail.com>
2024-01-26 22:01:39 +0530

    commit new

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function beforeSubmit() {
   let outputdate = document.querySelector(".leadDateDatabase");
   let inputdate = document.querySelector(".inputdate");
 
-  console.log("inputdate>>>", typeof inputdate.value);
   let formateddate = new Date(inputdate.value).toLocaleDateString("en-IN");
   outputdate.value = formateddate;
   console.log("outputdate>>", outputdate.value);
